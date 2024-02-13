0244993112b922d9a8425cac858fac6107026e2c
sshinde <sshinde@travelzoo.com>
2024-02-12 20:11:36 -0500

    Added URL based DAG

--- Readme.md
+++ Readme.md
@@ -1,4 +1,5 @@
 
 ## Sample Commands
 - Delete Task: airflow tasks test ml_delete_collection delete_collection_task
-- Index Tas: airflow tasks test ml_sitemap_pipeline parse_and_save_sitemap_embedings
+- Index Sitemap Task: airflow tasks test ml_sitemap_pipeline parse_and_save_sitemap_embedings
+- Index URL Task: airflow tasks test ml_url_parser_pipeline parse_and_save_url_embedings
--- dags/web_scraper_url_dag.py
+++ dags/web_scraper_url_dag.py
@@ -13,7 +13,7 @@ from airflow.operators.python import PythonOperator
 from machine_learning.utils.web_scraper import WebScraperEmbedder
 
 with DAG(
-    "ml_sitemap_pipeline",
+    "ml_url_parser_pipeline",
     # These args will get passed on to each operator
     # You can override them on a per-task basis during operator initialization
     default_args={
@@ -41,8 +41,7 @@ with DAG(
     catchup=False,
     tags=["machine_learning"],
     params={
-         "sitemap_url": Param("https://www.netcentric.biz/sitemap.xml", type=["null", "string"]),
-         "max_url_to_process": Param(10, type=["null", "integer"]),
+         "url_list": Param("https://js.langchain.com/docs/get_started/introduction,https://js.langchain.com/docs/get_started/installation", type=["null", "string"]),
          "chromadb_host_url": Param('10.0.1.104', type=["null", "string"]),
          "chromadb_collection_name": Param('a-test-collection', type=["null", "string"])
      }    
@@ -56,15 +55,23 @@ with DAG(
     )
 
     def create_emebeddings(**context):
-        sitemap_url = context["params"]["sitemap_url"]
-        max_url_to_process = context["params"]["max_url_to_process"]
-        print(f"Sitemap URL from DAG {sitemap_url}")
-        scrapper = WebScraperEmbedder(sitemap_url=sitemap_url, max_url_to_process=max_url_to_process)
-        return scrapper.parse_and_save_sitemap_embedings()
+        url_list = context["params"]["url_list"]
+        chromadb_host = context["params"]["chromadb_host_url"]
+        collection_name = context["params"]["chromadb_collection_name"]  
+        print(f"Indexing URLs {url_list}")
+        
+        if(url_list):
+            scrapper = WebScraperEmbedder(chromadb_host=chromadb_host,collection_name=collection_name)
+            urls = url_list.split(",")
+            for url in urls:
+                print(f"Creating Embeddings for URLs {url}")
+                documents = scrapper.parse_html_using_webloader(url=url)
+                scrapper.save_and_return_vector_store(documents)  
+                                                
     
     # Task: Scrape URLs and write to a file
     t2 = PythonOperator(
-        task_id='parse_and_save_sitemap_embedings',
+        task_id='parse_and_save_url_embedings',
         python_callable=create_emebeddings,
         dag=dag,
     )
