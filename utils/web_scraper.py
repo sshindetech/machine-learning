ef3c9a946a6732d544403a9ce834ccb0e3163cb5
sshinde <sshinde@travelzoo.com>
2024-02-22 00:11:20 -0500

    Added sample for image embeddig

--- utils/web_scraper.py
+++ utils/web_scraper.py
@@ -10,6 +10,13 @@ from langchain_community.vectorstores import Chroma
 from langchain.document_loaders.web_base import (WebBaseLoader,)
 from langchain_community.embeddings import HuggingFaceEmbeddings
 
+import os
+from pathlib import Path
+
+import pypdfium2 as pdfium
+from langchain_community.vectorstores import Chroma
+from langchain_experimental.open_clip import OpenCLIPEmbeddings
+
 class ChromDBClient:
     
     def __init__(self):
@@ -38,6 +45,20 @@ class ChromDBClient:
             }
         )
         return vectorStore
+    
+    def save_and_return_image_vectorstore(self):        
+        # Load embedding function
+        print("Loading embedding function")  # noqa: T201
+        embedding = OpenCLIPEmbeddings(model_name="ViT-H-14", checkpoint="laion2b_s32b_b79k")
+
+        # Create chroma
+        vectorstore_mmembd = Chroma(
+            client=self.chroma_client, 
+            collection_name="multi-modal-rag",
+            embedding_function=embedding,
+        )
+                
+        return vectorstore_mmembd    
 
 
 class WebScraperEmbedder(ChromDBClient):
@@ -104,7 +125,52 @@ class WebScraperEmbedder(ChromDBClient):
         
         logging.info(f"** COMPLETED LOADING SITEMAP FROM: {self.sitemap_url}")
 
+class ImageEmbedder(ChromDBClient):
+    def __init__(self):
+        super.__init__()
+            
+    def get_images_from_pdf(self, pdf_path, img_dump_path):
+        """
+        Extract images from each page of a PDF document and save as JPEG files.
 
+        :param pdf_path: A string representing the path to the PDF file.
+        :param img_dump_path: A string representing the path to dummp images.
+        """
+        pdf = pdfium.PdfDocument(pdf_path)
+        n_pages = len(pdf)
+        for page_number in range(n_pages):
+            page = pdf.get_page(page_number)
+            bitmap = page.render(scale=1, rotation=0, crop=(0, 0, 0, 0))
+            pil_image = bitmap.to_pil()
+            pil_image.save(f"{img_dump_path}/img_{page_number + 1}.jpg", format="JPEG")
+    
+    def embedded(self):
+        # Load PDF
+        doc_path = Path(__file__).parent / "docs/DDOG_Q3_earnings_deck.pdf"
+        img_dump_path = Path(__file__).parent / "docs/"
+        rel_doc_path = doc_path.relative_to(Path.cwd())
+        rel_img_dump_path = img_dump_path.relative_to(Path.cwd())
+        print("pdf index")  # noqa: T201
+        
+        pil_images = self.get_images_from_pdf(rel_doc_path, rel_img_dump_path)
+        print("done")  # noqa: T201
+        
+        # vectorstore = Path(__file__).parent / "chroma_db_multi_modal"
+        # re_vectorstore_path = vectorstore.relative_to(Path.cwd())
+
+        vectorstore_mmembd = self.save_and_return_image_vectorstore()
+        # Get image URIs
+        image_uris = sorted(
+            [
+                os.path.join(rel_img_dump_path, image_name)
+                for image_name in os.listdir(rel_img_dump_path)
+                if image_name.endswith(".jpg")
+            ]
+        )
+
+        # Add images
+        print("Embedding images")  # noqa: T201
+        vectorstore_mmembd.add_images(uris=image_uris)        
 
 # WebScraperEmbedder().parse_and_save_sitemap_embedings()
         
