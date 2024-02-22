6c3d150b5456e234519fead0912ebc185ed3c307
sshinde <sshinde@travelzoo.com>
2024-02-22 00:56:24 -0500

    test

--- utils/web_scraper.py
+++ utils/web_scraper.py
@@ -153,7 +153,7 @@ class ImageEmbedder(ChromDBClient):
         
         print(f"doc_path: {doc_path}")
         print(f"Path.cwd(): {Path.cwd()}")
-        print(f"Relative: {doc_path.relative_to(Path.cwd())}")        
+        # print(f"Relative: {doc_path.relative_to(Path.cwd())}")        
         rel_doc_path = '/home/allquill/airflow/dags/machine_learning/dags/docs' #doc_path.relative_to(Path.cwd())
         rel_img_dump_path = '/home/allquill/airflow/dags/machine_learning/dags/docs' #img_dump_path.relative_to(Path.cwd())
         print("pdf index")  # noqa: T201
