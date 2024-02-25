4190dfba86bf0dbc987c042462a4494a72f4a47d
sshinde <sshinde@travelzoo.com>
2024-02-24 19:30:07 -0500

    test

--- utils/clip_embeddings.py
+++ utils/clip_embeddings.py
@@ -56,7 +56,7 @@ class CLIPEmbeddings(BaseModel, Embeddings):
         print('image')
         print(image)
         
-        image_inputs = processor(images=image, return_tensors="pt")      
+        image_inputs = processor(images=image)      
         print('image_inputs')
         print(image_inputs)
         
