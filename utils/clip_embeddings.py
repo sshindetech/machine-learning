f58d847f0075b448063575bd1d89c4a20796e332
sshinde <sshinde@travelzoo.com>
2024-02-24 19:50:29 -0500

    test

--- utils/clip_embeddings.py
+++ utils/clip_embeddings.py
@@ -71,7 +71,7 @@ class CLIPEmbeddings(BaseModel, Embeddings):
         print('embeddings.image_embeds[0]')
         print(embeddings.image_embeds[0])  
                       
-        return embeddings.image_embeds[0].tolist()
+        return embeddings.image_embeds.tolist()
 
     def embed_query(self, text: str) -> List[float]:
         """Compute query embeddings using a HuggingFace transformer model.
